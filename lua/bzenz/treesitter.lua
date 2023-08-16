0959cc7eebf5c08bcf39a55163e314c42aac0a94
Benito Zenz <benito.zenz@data-experts.de>
2023-08-16 12:58:04 +0100

    FIX: Treesitter ignore svelte (install breaks)

--- lua/bzenz/treesitter.lua
+++ lua/bzenz/treesitter.lua
@@ -3,7 +3,7 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = "all",
   sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "svelte" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
