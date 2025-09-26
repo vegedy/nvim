4779c376c993325b36e67af8356cb29731e0c8b4
Benito Zenz <benito.zenz@data-experts.de>
2025-09-26 13:21:56 +0200

    Quality of life (which-key, autopairs, comment, surround, visual-multi)

---
+++ lua/benito/plugins/qol.lua
@@ -0,0 +1,8 @@
+return {
+  { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+  { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+  { "numToStr/Comment.nvim", event = "VeryLazy", opts = {} },
+  { "kylechui/nvim-surround", event = "VeryLazy", opts = {} }, -- ys{motion}{char} (add), ds{char} (delete), cs{target}{replacement} (change)
+  { "mg979/vim-visual-multi", branch = "master", event = "VeryLazy" }, -- Ctrl-N, n/N, q/Q
+}
+
