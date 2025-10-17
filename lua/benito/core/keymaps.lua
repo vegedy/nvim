9120d3a3c8e5152773b370739a4a3608cebefbe6
Benito Zenz <benito.zenz@data-experts.de>
2025-10-17 08:40:50 +0200

    Keymaps: bufferstepping, last char deletion

--- lua/benito/core/keymaps.lua
+++ lua/benito/core/keymaps.lua
@@ -1,3 +1,11 @@
 local map = vim.keymap.set
 map("n", "<leader>qh", ":nohlsearch<CR>", { desc = "Clear search highlight" })
+map("n", "<leader>bh", ":bp<CR>", { desc = "Go to previous buffer" })
+map("n", "<leader>bl", ":bn<CR>", { desc = "Go to next buffer" })
+
+vim.keymap.set('v', '<leader>x', ':norm $x<CR>', {
+    desc = "Delete last character in visual selection",
+    noremap = true,
+    silent = true
+})
 
